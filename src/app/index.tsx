a7429466e1181c267b69565787ba3b5ca4623d9a
Douglas Faria <hfsdouglas@gmail.com>
2024-08-15 19:50:17 -0300

    feat: add products section list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
-import { FlatList, View } from "react-native";
+import { useState, useRef } from "react"
+import { FlatList, View, SectionList, Text } from "react-native"
+import { Link } from "expo-router"
 
-import { CategoryButton } from "@/components/category-button";
-import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button"
+import { Header } from "@/components/header"
+import { Product } from "@/components/product"
 
-import { CATEGORIES } from "@/utils/data/products"
+import { CATEGORIES, MENU } from "@/utils/data/products"
 
 export default function Home() {
     const [category, setCategory] = useState(CATEGORIES[0])
 
+    const sectionListRef = useRef<SectionList>(null)
+
     function handleCategorySelect(selectedCategory: string) {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+        if (sectionListRef.current) {
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex, 
+                itemIndex: 0
+            })
+        }
     }
 
     return (
@@ -25,6 +39,24 @@ export default function Home() {
                 className="max-h-10 mt-5"
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
                 showsHorizontalScrollIndicator={false}
+            />
+
+            <SectionList 
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({ item }) => (
+                    <Link href={`/product/${item.id}`} asChild>
+                        <Product data={item} />
+                    </Link>
+                )}
+                renderSectionHeader={({ section: {title} }) => (
+                    <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>
+                )}
+                className="flex-1 p-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             />
         </View>
     )
