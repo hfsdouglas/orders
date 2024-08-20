9dffd2b8e2c5d4726b515bce855f29400c548e31
Douglas Faria <hfsdouglas@gmail.com>
2024-08-19 21:37:53 -0300

    fix: add title on product details screen

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -32,6 +32,8 @@ export default function Product() {
             <Image source={product.cover} className="w-full h-52" resizeMode="cover" />
 
             <View className="p-5 mt-8 flex-1">
+                <Text className="text-white text-xl font-heading">{product.title}</Text>
+                
                 <Text className="text-lime-400 text-2xl font-heading my-2">{formatCurrency(product.price)}</Text>
 
                 <Text className="text-slate-400 font-body text-base leading-6 mb-6">{product.description}</Text>
