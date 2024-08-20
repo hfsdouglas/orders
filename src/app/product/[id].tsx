d8583045d140244dad7f4e611d9da714fdd4d80c
Douglas Faria <hfsdouglas@gmail.com>
2024-08-19 21:32:49 -0300

    fix: find item on products array and redirect to root path if product doesnt exist

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View } from "react-native"
-import { useLocalSearchParams, useNavigation } from "expo-router"
+import { useLocalSearchParams, useNavigation, Redirect } from "expo-router"
 import { Feather } from "@expo/vector-icons"
 
 import { useCartStore } from "@/stores/cart-store"
@@ -14,11 +14,17 @@ export default function Product() {
     const navigation = useNavigation()
     const { id } = useLocalSearchParams()
 
-    const product = PRODUCTS.filter((item) => item.id === id)[0]
+    const product = PRODUCTS.find((item) => item.id === id)
+
+    if (!product) {
+        return <Redirect href="/" />
+    }
 
     function handleAddToCart() {
-        cartStore.add(product)
-        navigation.goBack()
+        if (product) {
+            cartStore.add(product)
+            navigation.goBack()
+        }
     }
 
     return (
